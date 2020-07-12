d84cc9b59759f6ab4dc58b0768e2986f76168bb5
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2020-07-12 12:10:23 +0530

    styled the Question Widget

--- quizapp/lib/question.dart
+++ quizapp/lib/question.dart
@@ -7,8 +7,16 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      questionText,
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10.0),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 28.0,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
