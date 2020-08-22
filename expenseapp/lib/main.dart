07a42f310e7cb0e31722a41acf6c610194d8dfaa
Aditya Patel <52347812+DeveloperAditya@users.noreply.github.com>
2020-08-22 17:14:51 +0530

    added a transaction model and transaction data

--- expenseapp/lib/main.dart
+++ expenseapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenseapp/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,6 +29,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
